fa791050bf1fcf13ecad4db202fb7d3baecfa7de
unni <39904158+labor-rice@users.noreply.github.com>
2022-03-04 10:48:03 +0800

    Update cli.js

--- cli.js
+++ cli.js
@@ -45,7 +45,7 @@ program
 program
   .on('--help', () => {
     // 新增说明信息
-    console.log(`\r\nRun ${chalk.cyan(`ch-cli <command> --help`)} for detailed usage of given command\r\n`)
+    console.log(`\r\nRun ${chalk.cyan(`labor-cli <command> --help`)} for detailed usage of given command\r\n`)
   })
 
 // 打印 logo
@@ -60,7 +60,7 @@ program
       whitespaceBreak: true
     }));
     // 新增说明信息
-    console.log(`\r\nRun ${chalk.cyan(`ch-cli <command> --help`)} show details\r\n`)
+    console.log(`\r\nRun ${chalk.cyan(`labor-cli <command> --help`)} show details\r\n`)
   })
 
 // 解析用户执行命令传入参数
